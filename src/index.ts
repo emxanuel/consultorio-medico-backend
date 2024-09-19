9b304e008e1f6f60fb56265b4cf97839a3d6bdb8
emxanuel <ealg0528@gmail.com>
2024-09-19 16:48:04 -0400

    add .dockerignore

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,6 @@ app.get("/api", (req, res) => {
   res.json("Hello World!");
 });
 
-app.listen(80, () => {
-  console.log("Server is running on port 80");
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port", process.env.PORT);
 });
